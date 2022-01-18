fe26a759cfb919b376186df7d15b07796e6bfb92
Manuel Samudio <manuel.ricardo.samudio@gmail.com>
2022-01-18 17:58:36 -0500

    :cat2: Add cat model and migrations

---
+++ app/Models/Cat.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Cat extends Model
+{
+    use HasFactory, Uuids, SoftDeletes;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function breed()
+    {
+        return $this->belongsTo(Breed::class);
+    }
+}
